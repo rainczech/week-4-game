c683e285e029059788dc592c63f2bd0e1d43c205
Grace Ho <graceho@Graces-MacBook-Pro.local>
2018-02-21 20:50:39 -0600

    assigned each gem an ID own math.random and onclick function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,33 +7,50 @@ var losses = "";
 // =====
 
 // give each gem a random value
-for(var i=0; i < 4; i++) {
-    var gemNumber = Math.floor(Math.random()*11+1);
-    console.log(gemNumber);
+// for loop commented out //
+// for(var i=0; i < 4; i++) {
+    // var gemNumber = Math.floor(Math.random()*11+1);
+    // console.log(gemNumber);
 
     // crystal = $(".gems");
     // crystal.attr({
     //     "data": gemNumber
-    };
+    // });
+// };
 
-// crystal values when clicked
-$(".gems").on("click", function() {
-    console.log($(this).attr(crystal.data));
+var gem1 = {
+    points: Math.floor(Math.random()*11+1),
+};
+
+var gem2 = {
+    points: Math.floor(Math.random()*11+1),
+};
+
+var gem3 = {
+    points: Math.floor(Math.random()*11+1),
+};
+
+var gem4 = {
+    points: Math.floor(Math.random()*11+1),
+};
+
+$("#gem1").on("click", function () {
+    console.log(gem1.points);
 });
 
+$("#gem2").on("click", function () {
+    console.log(gem2.points);
+});
 
+$("#gem3").on("click", function () {
+    console.log(gem3.points);
+});
 
-
+$("#gem4").on("click", function () {
+    console.log(gem4.points);
+});
 
 // produce random goal number between 19-120
-
-
-
-// var goalScore = Math.floor(Math.random()* 20);
-// goalScore=$("goalScore");
-// goalScore.toString.text();
-//random number for crystals
-
 
 
 
@@ -45,7 +62,7 @@ $(".gems").on("click", function() {
 
 
     var goalScore= Math.floor(Math.random()*101+19);
-    $("#goalScore").html("Your goal score is: " + goalScore);
+    $("#goalScore").html("Your number goal is: " + goalScore);
     console.log(goalScore);
 
 
